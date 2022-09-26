dec97c60f8202863c1d8fd0c13643858432f58df
BjorRiz <114378228+Farizz19@users.noreply.github.com>
2022-09-26 11:46:04 +0700

    JavaScript - #1
    JavaScript #1

---
+++ JavaScript/js/script.js
@@ -0,0 +1,46 @@
+// //Variabel
+// var nama="Fariz";
+// var umur= 16;
+ 
+// //Document
+// document.write("Nama Saya : " + nama);
+// document.write("<br>");
+// document.write("Umur : " + umur);
+
+alert ("Selamat Datang di Website Kami")
+
+// var ytb = confirm("apakah anda mau ke situs youtube?");
+
+// if(ytb)
+// {
+//     window.location = "https://youtu.be/dQw4w9WgXcQ";
+// }
+
+// else
+// {
+//     document.write("Ok, sepertinya kamu betah disini");
+// }
+
+// var nama = prompt("siapa nama kamu? ");
+// document.write("Halo! ");
+// document.write("<br>");
+// document.write("Nama kamu " + nama + " ya?");
+
+// var nilai_a = 10;
+// var nilai_b = 20;
+
+// var hasil1 = nilai_a + nilai_b;
+// var hasil2 = nilai_a / nilai_b;
+// var hasil3 = nilai_a - nilai_b;
+
+// document.write(hasil1);
+// document.write("<br>");
+// document.write(hasil2);
+// document.write("<br>");
+// document.write(hasil3);
+
+// var alas = 8;
+// var tinggi = 8;
+
+// var luas = 0.5 * alas * tinggi;
+
